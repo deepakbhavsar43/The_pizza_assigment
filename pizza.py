617162ffd5565b2d32ba54663defb4145a5ae14e
deepakbhavsar43 <deepakbhavsar43@gmail.com>
2021-04-25 17:18:27 +0530

    Add : Modified class, added to arguments and get n set method for them, modified calcCost

--- pizza.py
+++ pizza.py
@@ -1,10 +1,17 @@
-class pizza:
+class DeluxePizza:
     size, cheeseTopping, pepperoniTopping, mushroomTopping = 0, 0, 0, 0
-    def __init__(self, size, cheeseTopping, pepperoniTopping, mushroomTopping):
+    stuffedWithCheese = False
+    veggieTopping = 0
+    numberOfPizzas = 0
+    
+    def __init__(self, size, cheeseTopping, pepperoniTopping, mushroomTopping, stuffedWithCheese = 0, veggieTopping = 0):
         self.size = size
         self.cheeseTopping = cheeseTopping
         self.pepperoniTopping = pepperoniTopping
         self.mushroomTopping = mushroomTopping
+        self.numberOfPizzas += 1
+        self.stuffedWithCheese = 0
+        self.veggieTopping = 0
 
     def getSize(self):
         print(self.size)
@@ -18,6 +25,15 @@ class pizza:
     def getMushroomTopping(self):
         print(self.mushroomTopping)
 
+    def getStuffedWithCheese(self):
+        print(self.stuffedWithCheese)
+
+    def getVeggieTopping(self):
+        print(self.veggieTopping)
+
+    def getNumberOfPizzas(self):
+        print(self.numberOfPizzas)    
+ 
     def setSize(self, size):
         self.size = size
 
@@ -30,31 +46,45 @@ class pizza:
     def setMushroomTopping(self, mushroomTopping):
         self.mushroomTopping = mushroomTopping
 
+    def setStuffedWithCheese(self, stuffedWithCheese):
+        self.stuffedWithCheese = stuffedWithCheese
+
+    def setVeggieTopping(self, veggieTopping):
+        self.veggieTopping = veggieTopping
+
     def calcCost(self):
         total_toppings = self.cheeseTopping + self.pepperoniTopping + self.mushroomTopping
         cost = 0
         if self.size.lower() == "small":
-            cost = 10 + (2 * total_toppings)
-
+            cost = 10 + (2 * total_toppings) + (3 * self.veggieTopping)
+            if self.stuffedWithCheese == 1:
+                cost = cost + 2
+            
         if self.size.lower() == "medium":
-            cost = 12 + (2 * total_toppings)
-
+            cost = 12 + (2 * total_toppings) + (3 * self.veggieTopping)
+            if self.stuffedWithCheese == 1:
+                cost = cost + 4
+            
         if self.size.lower() == "large":
-            cost = 14 + (2 * total_toppings)
-        
+            cost = 14 + (2 * total_toppings) + (3 * self.veggieTopping)
+            if self.stuffedWithCheese == 1:
+                cost = cost + 6
+            
         return cost.__str__()
 
     def pizza_ordered(self):
         size = self.size.__str__()
+        stuffedWithCheese = self.stuffedWithCheese.__str__()
         cheeseTopping = self.cheeseTopping.__str__()
         pepperoniTopping = self.pepperoniTopping.__str__()
         mushroomTopping = self.mushroomTopping.__str__()
-        return size, cheeseTopping, pepperoniTopping, mushroomTopping
+        veggieTopping = self.veggieTopping.__str__()
+        return size, stuffedWithCheese, cheeseTopping, pepperoniTopping, mushroomTopping, veggieTopping
 
 if __name__ == "__main__":
-    pz = pizza("large", 1, 2, 1)
+    pz = DeluxePizza("large", 1, 2, 1)
     cost = pz.calcCost()
     print("pizza bill : ", cost)
-    size, cheeseTopping, pepprtoniTopping, mushroomTopping = pz.pizza_ordered()
-    print(f"pizza ordered :\n\tSize: {size}\n\tcheeseTopping: {cheeseTopping}\n\tpepprtoniTopping: {pepprtoniTopping}\n\tmushroomTopping: {mushroomTopping}")
+    size, stuffedWithCheese, cheeseTopping, pepprtoniTopping, mushroomTopping, veggieTopping = pz.pizza_ordered()
+    print(f"pizza #\n\tPizza size: {size}\n\tCheese filled dough: {stuffedWithCheese}\n\tNumber of cheese topping: {cheeseTopping}\n\tNumber of pepperoni topping: {pepprtoniTopping}\n\tNumber of mushroom topping: {mushroomTopping}\n\tNumber of vegetable topping: {veggieTopping}")
            
